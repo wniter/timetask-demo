3d16b291704e6cadfffa33f5c5600fd8d9b1a026
Boolean <lyg2000@aliyun.com>
2022-07-17 23:40:20 +0800

    add
    xxl-job未配置好

--- src/main/java/com/example/timedtask/demo/quartz/Myscheduler.java
+++ src/main/java/com/example/timedtask/demo/quartz/Myscheduler.java
@@ -1,11 +1,46 @@
 package com.example.timedtask.demo.quartz;
 
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.util.concurrent.TimeUnit;
+
 /**
- * 还是几个问题，对谁进行
+ * 还是几个问题
+ * 对谁进行定时任务，基本数据类型还是引用数据类型
+ * 返回是什么，要执行什么定时任务
  */
 public class Myscheduler  {
 
-    public static void main(String[] args) {
+    /**
+     * JobDataMap实现了JDK的Map接口，可以以Key-Value的形式存储数据。
+     * JobDetail、Trigger都可以使用JobDataMap来设置一些参数或信息，
+     * Job执行execute()方法的时候，JobExecutionContext可以获取到JobExecutionContext中的信息
+     */
+    public static void main(String[] args) throws  Exception{
+        // 1、创建调度器Scheduler
+        SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+        Scheduler scheduler = schedulerFactory.getScheduler();
+        // 2、创建JobDetail实例，并与PrintWordsJob类绑定(Job执行内容)
+        JobDetail jobDetail = JobBuilder.newJob(PrintWordsJob.class)
+                .withIdentity("job1", "group1").build();
+        // 3、构建Trigger实例,每隔1s执行一次
+        // 3、构建Trigger实例,每隔1s执行一次
+        Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "triggerGroup1")
+                .startNow()//立即生效
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInSeconds(1)//每隔1s执行一次
+                        .repeatForever()).build();//一直执行
+
+        //4、执行
+        scheduler.scheduleJob(jobDetail, trigger);
+        System.out.println("--------scheduler start ! ------------");
+        scheduler.start();
+
+        //睡眠
+        TimeUnit.MINUTES.sleep(1);
+        scheduler.shutdown();
+        System.out.println("--------scheduler shutdown ! ------------");
 
     }
 }
--- src/main/java/com/example/timedtask/demo/quartz/PrintWordsJob.java
+++ src/main/java/com/example/timedtask/demo/quartz/PrintWordsJob.java
@@ -1,7 +1,27 @@
 package com.example.timedtask.demo.quartz;
 
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 /**
- *
+ *Job是Quartz中的一个接口，接口下只有execute方法，在这个方法中编写业务逻辑。
  */
-public class PrintWordsJob {
+public class PrintWordsJob implements Job {
+    /**
+     *  JobExecutionContext中包含了Quartz运行时的环境以及Job本身的详细数据信息。
+     *当Schedule调度执行一个Job的时候，就会将JobExecutionContext传递给该Job的execute()中，Job就可以通过JobExecutionContext对象获取信息。
+     * 具体信息看JobExecutionContext的api
+     */
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        //TODO
+        String printTime = new SimpleDateFormat("yy-MM-dd HH-mm-ss").format(new Date());
+        System.out.println("PrintWordsJob start at:" + printTime + ", prints: Hello Job-" + new Random().nextInt(100));
+        System.out.println("hello world -----------------------------");
+    }
 }
